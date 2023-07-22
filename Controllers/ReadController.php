418100cd8081c631b522b2b9213f4a574d5d434b
kvnrynfl <kevinreynaufal2004@gmail.com>
2023-07-22 20:46:23 +0700

    Add read controller for book

---
+++ Controllers/ReadController.php
@@ -0,0 +1,19 @@
+<?php
+
+include('../Handlers/ConnectionHandler.php');
+include('../Handlers/ResponseHandler.php');
+
+$query = "SELECT * FROM buku";
+$result = mysqli_query($conn, $query);
+
+$bookData = [];
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $bookData[] = $row;
+}
+
+if (count($bookData) > 0) {
+    constructResponse(200, 'success', 'Data fetched successfully', $bookData);
+} else {
+    constructResponse(404, 'failed', 'No data found');
+}
