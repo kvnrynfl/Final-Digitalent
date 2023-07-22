39393d3b5dc8bcbe7bbd00308a62094bef79b3a0
kvnrynfl <kevinreynaufal2004@gmail.com>
2023-07-22 20:46:41 +0700

    Add delete controller for book

---
+++ Controllers/DeleteController.php
@@ -0,0 +1,15 @@
+<?php
+
+include('../Handlers/ConnectionHandler.php');
+include('../Handlers/ResponseHandler.php');
+
+$id = $_POST['id'];
+
+$query = "DELETE FROM buku WHERE id = $id";
+$result = mysqli_query($conn, $query);
+
+if ($result) {
+    constructResponse(200, 'success', 'Book deleted successfully');
+} else {
+    constructResponse(400, 'failed', 'Failed to delete book');
+}
