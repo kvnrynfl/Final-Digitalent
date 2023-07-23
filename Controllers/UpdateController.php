d4dd61855ec3bc668b954acb68751fcc0ec72ef1
kvnrynfl <kevinreynaufal2004@gmail.com>
2023-07-23 16:52:37 +0700

    Fix update controller

--- Controllers/UpdateController.php
+++ Controllers/UpdateController.php
@@ -1,19 +1,29 @@
 <?php
 
-$id = $_POST['id'];
-$judul = $_POST['judul'];
-$pengarang = $_POST['pengarang'];
-$tahun_terbit = $_POST['tahun_terbit'];
-$isbn = $_POST['isbn'];
+if (isset($_GET['id']) && isset($_GET['judul']) && isset($_GET['pengarang']) && isset($_GET['tahun_terbit']) && isset($_GET['isbn'])) {
+    $id = $_GET['id'];
+    $judul = $_GET['judul'];
+    $pengarang = $_GET['pengarang'];
+    $tahun_terbit = $_GET['tahun_terbit'];
+    $isbn = $_GET['isbn'];
 
-$query = "UPDATE buku SET judul = '$judul', pengarang = '$pengarang', tahun_terbit = '$tahun_terbit', isbn = '$isbn' WHERE id = $id";
-$result = mysqli_query($conn, $query);
+    $query = "UPDATE buku SET judul = '$judul', pengarang = '$pengarang', tahun_terbit = '$tahun_terbit', isbn = '$isbn' WHERE id = '$id'";
+    $result = mysqli_query($conn, $query);
 
-if ($result) {
-    $updatedBookQuery = "SELECT * FROM buku WHERE id = $id";
-    $updatedBookResult = mysqli_query($conn, $updatedBookQuery);
-    $updatedBookData = mysqli_fetch_assoc($updatedBookResult);
-    constructResponse(200, 'success', 'Book updated successfully', $updatedBookData);
+    if ($result) {
+        // Book update successful, fetch the updated book data
+        $updatedBookQuery = "SELECT * FROM buku WHERE id = $id";
+        $updatedBookResult = mysqli_query($conn, $updatedBookQuery);
+
+        if ($updatedBookResult && mysqli_num_rows($updatedBookResult) > 0) {
+            $updatedBookData = mysqli_fetch_assoc($updatedBookResult);
+            constructResponse(200, 'success', 'Book updated successfully', $updatedBookData);
+        } else {
+            constructResponse(404, 'failed', 'Failed to fetch updated book data');
+        }
+    } else {
+        constructResponse(500, 'failed', 'Failed to update book');
+    }
 } else {
-    constructResponse(500, 'failed', 'Failed to update book');
+    constructResponse(400, 'failed', 'Incomplete or missing parameters');
 }
