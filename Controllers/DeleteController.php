f8aebee55940a9b228afb9e6857d41fcab000183
kvnrynfl <kevinreynaufal2004@gmail.com>
2023-07-23 16:52:44 +0700

    Fix delete controller

--- Controllers/DeleteController.php
+++ Controllers/DeleteController.php
@@ -1,12 +1,22 @@
 <?php
 
-$id = $_POST['id'];
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
 
-$query = "DELETE FROM buku WHERE id = $id";
-$result = mysqli_query($conn, $query);
+    $query = "DELETE FROM buku WHERE id = '$id'";
 
-if ($result) {
-    constructResponse(200, 'success', 'Book deleted successfully');
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+        $deletedRows = mysqli_affected_rows($conn);
+        if ($deletedRows > 0) {
+            constructResponse(200, 'success', 'Book deleted successfully');
+        } else {
+            constructResponse(404, 'failed', 'Book with the specified ID not found');
+        }
+    } else {
+        constructResponse(400, 'failed', 'Failed to delete book');
+    }
 } else {
-    constructResponse(400, 'failed', 'Failed to delete book');
+    constructResponse(400, 'failed', 'Failed to get ID book');
 }
