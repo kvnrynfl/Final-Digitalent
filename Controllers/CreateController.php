07cb074787d198422a5f3393fb92e6a39ff1fb26
kvnrynfl <kevinreynaufal2004@gmail.com>
2023-07-23 16:49:04 +0700

    Fix create controller

--- Controllers/CreateController.php
+++ Controllers/CreateController.php
@@ -1,19 +1,29 @@
 <?php
 
-$judul = $_POST['judul'];
-$pengarang = $_POST['pengarang'];
-$tahun_terbit = $_POST['tahun_terbit'];
-$isbn = $_POST['isbn'];
+if (isset($_GET['judul']) && isset($_GET['pengarang']) && isset($_GET['tahun_terbit']) && isset($_GET['isbn'])) {
+    $judul = $_GET['judul'];
+    $pengarang = $_GET['pengarang'];
+    $tahun_terbit = $_GET['tahun_terbit'];
+    $isbn = $_GET['isbn'];
 
-$query = "INSERT INTO buku (judul, pengarang, tahun_terbit, isbn) VALUES ('$judul','$pengarang','$tahun_terbit','$isbn`)";
-$result = mysqli_query($conn, $query);
+    $query = "INSERT INTO buku (judul, pengarang, tahun_terbit, isbn) VALUES ('$judul','$pengarang','$tahun_terbit','$isbn')";
+    $result = mysqli_query($conn, $query);
 
-if ($result) {
-    $newBookId = mysqli_insert_id($conn);
-    $newBookQuery = "SELECT * FROM buku WHERE id = $newBookId";
-    $newBookResult = mysqli_query($conn, $newBookQuery);
-    $newBookData = mysqli_fetch_assoc($newBookResult);
-    constructResponse(201, 'success', 'Book added successfully', $newBookData);
+    if ($result) {
+        // Book added successfully, fetch the newly added book data
+        $newBookId = mysqli_insert_id($conn);
+        $newBookQuery = "SELECT * FROM buku WHERE id = $newBookId";
+        $newBookResult = mysqli_query($conn, $newBookQuery);
+
+        if ($newBookResult && mysqli_num_rows($newBookResult) > 0) {
+            $newBookData = mysqli_fetch_assoc($newBookResult);
+            constructResponse(201, 'success', 'Book added successfully', $newBookData);
+        } else {
+            constructResponse(404, 'failed', 'Failed to fetch newly added book data');
+        }
+    } else {
+        constructResponse(500, 'failed', 'Failed to add book');
+    }
 } else {
-    constructResponse(500, 'failed', 'Failed to add book');
+    constructResponse(400, 'failed', 'Incomplete or missing parameters');
 }
